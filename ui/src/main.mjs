2377c950160c22f81a31d90df9c7f455ee3adc3b
cgjgh <160297365+cgjgh@users.noreply.github.com>
2024-04-26 21:10:55 -0500

    Dev Updates

--- ui/src/main.mjs
+++ ui/src/main.mjs
@@ -17,10 +17,10 @@ import * as components from 'vuetify/components'
 import * as labsComponents from 'vuetify/labs/components'
 import * as directives from 'vuetify/directives'
 
-// Labs Imports
-import { VNumberInput } from 'vuetify/labs/VNumberInput'
-import { VSparkline } from 'vuetify/labs/VSparkline'
-import { VTreeview } from 'vuetify/labs/VTreeview'
+// // Labs Imports
+// import { VNumberInput } from 'vuetify/labs/VNumberInput'
+// import { VSparkline } from 'vuetify/labs/VSparkline'
+// import { VTreeview } from 'vuetify/labs/VTreeview'
 
 import './stylesheets/common.css'
 
