dc1822370dd6b49170edd182330a8640f9b8f7c4
cgjgh <160297365+cgjgh@users.noreply.github.com>
2024-05-24 21:38:10 -0500

    Caching

--- replace-mdi-version.js
+++ replace-mdi-version.js
@@ -7,10 +7,10 @@ fs.readdirSync(distFolder).forEach((file) => {
     if (file.endsWith('.css')) {
         const filePath = path.join(distFolder, file)
         let cssContent = fs.readFileSync(filePath, 'utf8')
-        cssContent = cssContent.replace(/\?v=\d+\.\d+\.\d+/g, '') // Replace the query parameter with any version number
+        cssContent = cssContent.replace(/(woff|eot|woff2|ttf)\?v=\d+\.\d+\.\d+/g, '$1') // Replace the query parameter with any version number only if it's preceded by 'woff', 'eot', 'woff2', 'ttf'
         fs.writeFileSync(filePath, cssContent, 'utf8')
         console.log(`Updated ${file}`)
     }
 })
 
-console.log('CSS files updated successfully!')
+console.log('MDI cache invalidation successfully removed from CSS files!')
