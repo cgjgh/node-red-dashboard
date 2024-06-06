f881c9ea5c943e2c5bdb4d0d9fbc2289008b7a8a
cgjgh <160297365+cgjgh@users.noreply.github.com>
2024-06-06 14:07:33 -0500

    Auto Reload on focus

--- ui/src/main.mjs
+++ ui/src/main.mjs
@@ -81,126 +81,143 @@ const vuetify = createVuetify({
 // if our scoket disconnects, we should inform the user when it reconnects
 
 // GET our SocketIO Config from Node-RED & any other bits plugins have added to the _setup endpoint
-fetch('_setup')
-    .then(async (response) => {
-        const url = new URL(response.url)
-        const basePath = url.pathname.replace('/_setup', '')
+const fetchSetup = () => {
+    return fetch('_setup')
+        .then(async (response) => {
+            const url = new URL(response.url)
+            const basePath = url.pathname.replace('/_setup', '')
 
-        // get the setup JSON from the server
-        const setup = await response.json()
-        setup.basePath = basePath
-
-        if (setup.socketio?.path) {
-            // get text before /socket.io and replace it with the calculated basePath
-            // basePath would have taken into account any proxy and/or httpNodeRoot settings
-            const replace = setup.socketio.path.split('/socket.io')[0]
-            setup.socketio.path = setup.socketio.path.replace(replace, basePath)
-        }
-
-        store.commit('setup/set', setup)
-
-        let disconnected = false
-        let disconnectedAt = null
-
-        let reconnectTO = null
-        const MAX_TIMER = 300000 // 5 minutes
-
-        const socket = io({
-            ...setup.socketio,
-            reconnection: false
-        })
-
-        // handle final disconnection
-        socket.on('disconnect', (reason) => {
-            if (!disconnected) {
-                disconnectedAt = new Date()
-                disconnected = true
+            // Check if the response was redirected
+            if (response.redirected) {
+                console.log('Response was redirected to auth')
+            } else {
+                console.log('Response came from the original request source')
             }
-            store.commit('ui/connectionStatus', false)
-            // tell the user we're trying to connect
-            Alerts.emit('Connection Lost', 'Attempting to reconnect to server...', 'red', {
-                displayTime: 0,
-                allowDismiss: false,
-                showCountdown: false
+
+            // get the setup JSON from the server
+            const setup = await response.json()
+            setup.basePath = basePath
+
+            if (setup.socketio?.path) {
+                // get text before /socket.io and replace it with the calculated basePath
+                // basePath would have taken into account any proxy and/or httpNodeRoot settings
+                const replace = setup.socketio.path.split('/socket.io')[0]
+                setup.socketio.path = setup.socketio.path.replace(replace, basePath)
+            }
+
+            store.commit('setup/set', setup)
+
+            let disconnected = false
+            let disconnectedAt = null
+
+            let reconnectTO = null
+            const MAX_TIMER = 300000 // 5 minutes
+
+            const socket = io({
+                ...setup.socketio,
+                reconnection: false
             })
-            // attempt to reconnect
-            reconnect()
-        })
 
-        socket.on('connect', () => {
-            console.log('SIO connected')
-            store.commit('ui/connectionStatus', true)
-            // if we've just disconnected (i.e. aren't connecting for the first time)
-            if (disconnected) {
-                // send a notification/alert to the user to let them know the connection is live again
-                Alerts.emit('Connected', 'Connection re-established.', '#1BC318', {
-                    displayTime: 3,
-                    allowDismiss: true,
-                    showCountdown: true
+            // handle final disconnection
+            socket.on('disconnect', (reason) => {
+                if (!disconnected) {
+                    disconnectedAt = new Date()
+                    disconnected = true
+                }
+                store.commit('ui/connectionStatus', false)
+                // tell the user we're trying to connect
+                Alerts.emit('Connection Lost', 'Attempting to reconnect to server...', 'red', {
+                    displayTime: 0,
+                    allowDismiss: false,
+                    showCountdown: false
                 })
-            }
-            disconnected = false
-            clearTimeout(reconnectTO)
-        })
+                // attempt to reconnect
+                reconnect()
+            })
 
-        socket.on('connect_error', (err) => {
-            console.error('SIO connect error:', err, err.data)
-        })
-
-        // default interval - every 5 seconds
-        function reconnect (interval = 5000) {
-            if (disconnected) {
-                socket.connect()
-                const now = new Date()
-                if (now - disconnectedAt > 60000) {
-                    // trying for over 1 minute
-                    interval = 30000 // interval at 30 seconds
+            socket.on('connect', () => {
+                console.log('SIO connected')
+                store.commit('ui/connectionStatus', true)
+                // if we've just disconnected (i.e. aren't connecting for the first time)
+                if (disconnected) {
+                    // send a notification/alert to the user to let them know the connection is live again
+                    Alerts.emit('Connected', 'Connection re-established.', '#1BC318', {
+                        displayTime: 1,
+                        allowDismiss: true,
+                        showCountdown: true
+                    })
                 }
-                // if still within our maximum timer
-                if (now - disconnectedAt < MAX_TIMER) {
-                    // check for a connection again in <interval> milliseconds
-                    reconnectTO = setTimeout(reconnect, interval)
+                disconnected = false
+                clearTimeout(reconnectTO)
+            })
+
+            socket.on('connect_error', (err) => {
+                console.error('SIO connect error:', err, err.data)
+            })
+
+            // default interval - every 5 seconds
+            function reconnect (interval = 5000) {
+                if (disconnected) {
+                    socket.connect()
+                    const now = new Date()
+                    if (now - disconnectedAt > 60000) {
+                        // trying for over 1 minute
+                        interval = 30000 // interval at 30 seconds
+                    }
+                    // if still within our maximum timer
+                    if (now - disconnectedAt < MAX_TIMER) {
+                        // check for a connection again in <interval> milliseconds
+                        reconnectTO = setTimeout(reconnect, interval)
+                    }
                 }
             }
-        }
 
-        /**
+            /**
          * Create VueJS App
          */
-        window.Vue = Vue // make VueJS available globally for third-party NR widgets
-        window.vuex = vuex // make Vuex available globally for third-party NR widgets
-        const app = Vue.createApp(App)
-            .use(store)
-            .use(vuetify)
-            .use(router)
+            window.Vue = Vue // make VueJS available globally for third-party NR widgets
+            window.vuex = vuex // make Vuex available globally for third-party NR widgets
+            const app = Vue.createApp(App)
+                .use(store)
+                .use(vuetify)
+                .use(router)
 
-        const head = createHead()
-        app.use(head)
-        app.mixin(VueHeadMixin)
+            const head = createHead()
+            app.use(head)
+            app.mixin(VueHeadMixin)
 
-        // make the socket service available app-wide via this.$socket
-        app.provide('$socket', socket)
+            // make the socket service available app-wide via this.$socket
+            app.provide('$socket', socket)
 
-        // mount the VueJS app into <div id="app"></div> in /ui/public/index.html
-        app.mount('#app')
-    })
-    .catch((err) => {
-        if (err instanceof TypeError) {
-            if (err.message === 'Failed to fetch') {
-                console.log('auth error:', err)
-                console.log('redirecting to:', window.location.origin + '/dashboard')
+            // mount the VueJS app into <div id="app"></div> in /ui/public/index.html
+            app.mount('#app')
+        })
+        .catch((err) => {
+            if (err instanceof TypeError) {
+                if (err.message === 'Failed to fetch') {
+                    console.log('auth error:', err)
+                    console.log('redirecting to:', window.location.origin + '/dashboard')
 
-                // window.location.replace(window.location.origin + '/dashboard') //Original, seems to have no issues with Edge and Chrome on Windows, doesn't work on Android (Not tested on Linux browser)
-                // window.location.href = window.location.origin + window.location.pathname + window.location.search + (window.location.search ? '&' : '?') + 'reloadTime=' + Date.now().toString() + window.location.hash; // Also works on Edge + Chrome on windows, doesn't work on android
+                    // window.location.replace(window.location.origin + '/dashboard') //Original, seems to have no issues with Edge and Chrome on Windows, doesn't work on Android (Not tested on Linux browser)
+                    // window.location.href = window.location.origin + window.location.pathname + window.location.search + (window.location.search ? '&' : '?') + 'reloadTime=' + Date.now().toString() + window.location.hash; // Also works on Edge + Chrome on windows, doesn't work on android
 
-                // Reloading dashboard without using cache by apending a cache-busting string to fully reload page to allow redirecting to auth
-                window.location.replace(window.location.origin + '/dashboard' + '?' + 'reloadTime=' + Date.now().toString() + Math.random()) // Seems to work on Edge and Chrome on Windows, Chromium and Firefox on Linux, and also on Chrome Android (and also as PWA App)
+                    // Reloading dashboard without using cache by apending a cache-busting string to fully reload page to allow redirecting to auth
+                    window.location.replace(window.location.origin + '/dashboard' + '?' + 'reloadTime=' + Date.now().toString() + Math.random()) // Seems to work on Edge and Chrome on Windows, Chromium and Firefox on Linux, and also on Chrome Android (and also as PWA App)
+                } else {
+                    // handle general Type errors here
+                    console.error('An error occurred:', err)
+                }
             } else {
-                // handle general Type errors here
+                // handle general errors here
                 console.error('An error occurred:', err)
             }
-        } else {
-            // handle general errors here
-            console.error('An error occurred:', err)
-        }
-    })
+        })
+}
+
+fetchSetup()
+
+window.addEventListener('focus', () => {
+    if (!store.state.ui.connectionStatus) {
+        fetchSetup()
+    }
+})
